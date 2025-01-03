8642d6f135ed19b023972a925ed22cebfa961b81
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2025-01-03 22:33:22 +0300

    Create module_3_1.py

---
+++ module_3_1.py
@@ -0,0 +1,21 @@
+# Функция count_calls - количество всех других функций, кроме count_calls
+def count_calls():
+    global calls
+    calls += 1
+# Функция длины переданной строки, строка.upper(), строка.lower()
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+# Функция проверки наличия строки в списке независимо от регистра
+def is_contains(string, list_to_search):
+    count_calls()
+    list_to_search = [i.lower() for i in list_to_search]  # весь список в нижний регистр
+    return (string.lower() in
+            [j.lower() for j in list_to_search])  # строка тоже в нижний регистр
+# Вывод результатов
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)

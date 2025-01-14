7bd3526db1f245b7d38dd792c639996603187c08
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2025-01-14 04:51:06 +0300

    Update module_3_6.py

--- module_3_6.py
+++ module_3_6.py
@@ -1,1 +1,34 @@
+def calculate_structure_result(*args): # recursive counting function
+    global call_counter
+    call_counter += 1
+    result = 0
+    for x in args:
+        if isinstance(x, int):  # count all integers
+            result += x
+        elif isinstance(x, str):  # sum up the lengths of all strings
+            result += len(x)
+        elif isinstance(x, (list, tuple,
+                            set)):  # recursively counting nested elements in lists, tuples and sets
+            result += calculate_structure_result(*x)
+        elif isinstance(x, dict):  # recursively counting elements in a dictionary
+            result += calculate_structure_result(*tuple(x.items()))
+    return result
 
+
+call_counter = 0
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+print(f'{calculate_structure_result(data_structure)} - это результат \nрекурсивного '
+      f'подсчета\nвсевозможных чисел')
+print(f'{call_counter} - вызовов функции')
+
+consol:
+# 99 - это результат 
+# рекурсивного подсчета
+# всевозможных чисел
+# 16 - вызовов функции
